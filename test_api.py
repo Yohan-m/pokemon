27b6015d5ccde1d2626e84591ca9f1db3f46496f
Alexandre Zeghdoud <alexandre@imac-de-au.home>
2021-05-19 01:45:41 +0200

    v2 Alexandre

--- test_api.py
+++ test_api.py
@@ -1,5 +1,7 @@
 import pokebase as pb
 
+id = int(input('Rentrez un numéro de pokémon! '))-1
+
 class Pokemon:
     def __init__(self, id):
         self.name = pb.pokemon(id).name
@@ -8,6 +10,8 @@ class Pokemon:
         self.atk = pb.pokemon(id).stats[1].base_stat
         self.hp = pb.pokemon(id).stats[0].base_stat
         self.vit = pb.pokemon(id).stats[5].base_stat
+   
+
 
 
 # b = Pokemon(1)
@@ -17,24 +21,60 @@ class Pokemon:
 # boucle création pokémon api vers objet
 # le premier pokémon à pour indez 0 mais son id dans l'api est 1
 pokelist = []
-for i in range(1,13,1):
+for i in range(1,10,1):
     onepok = Pokemon(i)
     pokelist.append(onepok)
+#Quiz nom pokémon
+mot = pokelist[id].name
+lettre = mot[:1]
+secLetter = mot[:2]
 
+taillePoke = str(pokelist[id].height*10)
+
+print("La taille de ce pokémon est : " + str(taillePoke)+' cm')
+print("Ce pokémon est de type : " + str(pokelist[id].type))
+print("Les stats d'attack de ce pokémon est de : "+ str(pokelist[id].atk))
+print("Les stats de vie de ce pokémon est de : "+ str(pokelist[id].hp))
+print("La vitesse de ce pokémon est de : "+ str(pokelist[id].vit))
+print("La premiere lettre du prénom de ce pokémon lettre est : " + str(lettre))
+entryUser = input('Rentrez le nom du pokémon : ')
+
+i= True
+while i:
+    if str(entryUser) == str(pokelist[id].name):
+        print('Bien joué le pokémon était : ' + str(pokelist[id].name))
+        break
+    else:
+        questSecLetter= input('Voulez vous la 2ème lettre de ce pokémon ? - y or n ')
+
+    if questSecLetter == 'y':
+        print("La deuxième lettre du prénom de ce pokémon lettre est : " + str(secLetter))
+        entryUser = input('Rentrez le nom du pokémon : ')
+    else:
+        entryUser = input('Rentrez le nom du pokémon : ')
+    
+
+
+    
+    
+
+
+"""
 # boucle affiché les noms des pokémon
 j = 0
 
 while j < 12:
     
-    print("Le nom de pokémon : " + pokelist[j].name)
-    print("La taille de ce pokémon est : " + str(pokelist[j].height))
-    print("Ce pokémon est de type : " + str(pokelist[j].type))
-    print("Les stats d'attack de ce pokémon est de : "+ str(pokelist[j].atk))
-    print("Les stats de vie de ce pokémon est de : "+ str(pokelist[j].hp))
-    print("La vitesse de ce pokémon est de : "+ str(pokelist[j].vit))
+    print("Le nom de pokémon : " + pokelist[id].name)
+    print("La taille de ce pokémon est : " + str(pokelist[id].height))
+    print("Ce pokémon est de type : " + str(pokelist[id].type))
+    print("Les stats d'attack de ce pokémon est de : "+ str(pokelist[id].atk))
+    print("Les stats de vie de ce pokémon est de : "+ str(pokelist[id].hp))
+    print("La vitesse de ce pokémon est de : "+ str(pokelist[id].vit))
 
     print(" ")
     j = j+1
+"""
 
 # compléter class pokémon avec toutes les données qu'on a besoin
 # Faire une fonction hors class de tri genre affcihé que les pokémons commencant par un c (reduce ou filter) possibilité d'evoluer que ce soit l'utilistateur qui rentre la 1er lettre
