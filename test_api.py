25061abf2c980a3e66c1b85cc2370eb9000ba998
Yohan Marchand <y_marchand1@etu-webschoolfactory.fr>
2021-05-20 15:39:24 +0200

    consigne

--- test_api.py
+++ test_api.py
@@ -152,14 +152,11 @@ while panel != 5:
 print ("Merci d'avoir jouer !")
 
 
-# compléter class pokémon avec toutes les données qu'on a besoin --
-# Faire une fonction hors class de tri genre affcihé que les pokémons commencant par un c (reduce ou filter) possibilité d'evoluer que ce soit l'utilistateur qui rentre la 1er lettre
 # Faire une méthode afficher le nombre de pokémon par types (possibilité dico clé type et valuer le nombre)
-# Faire une fonction qui affiche le pokemon en donnant l'index
-# Faire une fonction donnant un pokemon aleatoire biblioeque math random (demandé)
-
-# Faire une sorte d'interface taper 1 pour entrer un index et voir les stat du pokémon taper 2 pour affiché les pokémon commencant par c...
-# Le programme ne doit pas s'arrêter donc à chaque fin d'execution on retourne à l'interface et c'est sur l'interface en tapant "close" que le programme s'arrete
+# fonction ou ajouter à la suite des fonctions affiché 1 pokémon générateur / itérateur pour afficher le pokémon suivant
 # faire les docs
 
-# Si on a le temps affiché les sorties avec la bibliothèque vu en cours
+# Si on a le temps essayer de regroupé tous les affichages des stats pokémon dans la fonction stat 
+# donc dans fonction quizz faire un appel à stat mais avec un parametre pour pas afficher le nom
+# pour ensuite affiché la fonction stat avec la bibliothèque vu en cours
+# ou directement faire la biblioteque à plusieur endrit dans le code si plus simple
